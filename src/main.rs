0bb58b68b6a3ec205d5b01fe54428f37947524d1
Nick Pillitteri <nickp@smartertravelmedia.com>
2018-09-28 11:03:46 -0400

    Start forwarding signals from parent to child

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ extern crate signal_hook;
 use crossbeam_channel::{Receiver, Sender};
 use libc::c_int;
 use signal_hook::iterator::Signals;
+use std::cell::Cell;
 use std::env;
 use std::process::{self, Command};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-fn notify(signals: &[c_int]) -> Receiver<c_int> {
+fn launch_signals(signals: &[c_int]) -> Receiver<c_int> {
     let (s, r) = crossbeam_channel::bounded(100);
     let signals = Signals::new(signals).unwrap();
+
     thread::spawn(move || {
         for signal in signals.forever() {
             s.send(signal);
@@ -28,24 +31,37 @@ fn main() {
         process::exit(1);
     }
 
-    let receiver = notify(&[
+    let receiver = launch_signals(&[
         signal_hook::SIGHUP,
         signal_hook::SIGTERM,
         signal_hook::SIGINT,
         signal_hook::SIGQUIT,
     ]);
 
+    let pid = Arc::new(Mutex::new(Cell::new(0)));
+    let our_pid = Arc::clone(&pid);
+
+    thread::spawn(move || {
+        for i in receiver {
+            let child_pid = our_pid.lock().unwrap().get();
+
+            if child_pid != 0 {
+                println!("Sending signal {:?} to {:?}", i, child_pid);
+
+                unsafe {
+                    libc::kill(child_pid, i as c_int);
+                };
+            } else {
+                println!("Invalid pid {:?}", child_pid);
+            }
+        }
+    });
+
     let mut child = Command::new(&args[1])
         .args(args[2..].iter())
         .spawn()
         .unwrap();
 
+    pid.lock().unwrap().set(child.id() as i32);
     child.wait().unwrap();
-
-    for i in receiver {
-        println!("Got signal {:?}", i);
-        if signal_hook::SIGTERM == i {
-            break;
-        }
-    }
 }
